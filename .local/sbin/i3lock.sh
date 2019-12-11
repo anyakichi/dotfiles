98290eaf2584fae76ad31fd5fd292be512bcc2e1
INAJIMA Daisuke <inajima@sopht.jp>
2019-12-11 16:22:13 +0900

    i3lock.sh: Show software keyboard if onboard is enabled

--- .local/sbin/i3lock.sh
+++ .local/sbin/i3lock.sh
@@ -19,6 +19,19 @@ post_lock() {
     return
 }
 
+show_software_keyboard() {
+    # Hide onboard once because it can be already shown
+    dbus-send --type=method_call --dest=org.onboard.Onboard \
+        /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Hide
+
+    # Wait i3lock locks the session
+    sleep 2
+
+    # Show onoard over i3lock
+    dbus-send --type=method_call --dest=org.onboard.Onboard \
+        /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show
+}
+
 ###############################################################################
 
 pre_lock
@@ -39,12 +52,15 @@ if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
     # now close our fd (only remaining copy) to indicate we're ready to sleep
     exec {XSS_SLEEP_LOCK_FD}<&-
 
+    show_software_keyboard
+
     while kill_i3lock -0; do
         sleep 0.5
     done
 else
     trap 'kill %%' TERM INT
     i3lock -n $i3lock_options "$@" &
+    show_software_keyboard
     wait
 fi
 
