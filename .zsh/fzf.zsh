2e37bfbe1a147524c5f249cdcf22a13ec079a612
INAJIMA Daisuke <inajima@gmail.com>
2021-02-16 23:42:16 +0900

    fzf.zsh: minor improvements

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,13 +1,8 @@
-is_in_git_repo() {
-    git rev-parse HEAD >/dev/null 2>&1
-}
-
 fzf-down() {
     fzf --height 50% "$@" --border
 }
 
 _gb() {
-    is_in_git_repo || return
     fzf-git-log init
     git branch -a --color=always | command grep -v '/HEAD\s' \
         | fzf-down --ansi --multi --preview-window right:70% \
@@ -18,31 +13,35 @@ _gb() {
 }
 
 _gf() {
-    is_in_git_repo || return
     git -c color.status=always status --short \
         | fzf-down -m --ansi --nth 2..,.. \
             --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' \
         | cut -c4- | sed 's/.* -> //'
 }
 
-_gh() {
-    is_in_git_repo || return
+gh() {
     fzf-git-log init
-    fzf-git-log run \
+    fzf-git-log run "$@" \
         | fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
             --bind 'ctrl-g:execute-silent(fzf-git-log toggle-graph)+reload(fzf-git-log run)' \
-            --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES \
+            --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -500' \
         | command grep -o "[a-f0-9]\{7,\}"
+}
+
+_gh() {
+    gh
 }
 
 _gg() {
     RG="rg --column --line-number --no-heading --color=always --smart-case "
-    fzf --bind "change:reload:$RG {q} || true" --ansi --disabled \
+    fzf --ansi --disabled \
+        --bind "change:reload:$RG {q} || true" \
+        --preview 'fzf-find-preview $(cut -d ':' -f1 <<< {})' \
+        --preview-window hidden \
         | command cut -d ':' -f1
 }
 
 _gr() {
-    is_in_git_repo || return
     fzf-git-log init
     git remote -v | awk '{print $1 "\t" $2}' | uniq \
         | fzf-down --tac --preview 'fzf-git-log run {1} | head -200' \
@@ -51,10 +50,9 @@ _gr() {
 }
 
 _gt() {
-    is_in_git_repo || return
     git tag --sort -version:refname \
         | fzf-down --multi --preview-window right:70% \
-            --preview 'git show --color=always {} | head -'$LINES
+            --preview 'git show --color=always {} | head -500'
 }
 
 join-lines() {
