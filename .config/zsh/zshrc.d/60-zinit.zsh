42150c576a65c48470bd3f500434427d65c048cd
INAJIMA Daisuke <inajima@gmail.com>
2021-07-13 12:40:13 +0900

    zsh: Use XDG Base Directory

---
+++ .config/zsh/zshrc.d/60-zinit.zsh
@@ -0,0 +1,36 @@
+declare -A ZINIT
+
+ZINIT[HOME_DIR]="$XDG_DATA_HOME/zsh/zinit"
+ZINIT[BIN_DIR]="${ZINIT[HOME_DIR]}/bin"
+ZINIT[ZCOMPDUMP_PATH]="$XDG_CACHE_HOME/zsh/zcompdump"
+
+### Added by Zinit's installer
+if [[ ! -f "${ZINIT[BIN_DIR]}/zinit.zsh" ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "${ZINIT[HOME_DIR]}" && command chmod g-rwX "${ZINIT[HOME_DIR]}"
+    command git clone https://github.com/zdharma/zinit "${ZINIT[BIN_DIR]}" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+source "${ZINIT[BIN_DIR]}/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zinit-zsh/z-a-as-monitor \
+    zinit-zsh/z-a-patch-dl \
+    zinit-zsh/z-a-bin-gem-node
+
+### End of Zinit's installer chunk
+
+zinit light-mode for \
+    anyakichi/fzf-utils \
+    reegnz/jq-zsh-plugin \
+    romkatv/powerlevel10k \
+    zdharma/fast-syntax-highlighting \
+    zsh-users/zsh-autosuggestions \
+    zsh-users/zsh-completions \
+    zsh-users/zsh-history-substring-search
