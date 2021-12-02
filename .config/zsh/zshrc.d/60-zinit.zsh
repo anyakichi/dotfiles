236b633eefe96a3c36ae276ce453d3888d8858e7
INAJIMA Daisuke <inajima@gmail.com>
2021-12-02 12:51:44 +0900

    zsh: Switch to zdharma-continuum

--- .config/zsh/zshrc.d/60-zinit.zsh
+++ .config/zsh/zshrc.d/60-zinit.zsh
@@ -8,7 +8,7 @@ ZINIT[ZCOMPDUMP_PATH]="$XDG_CACHE_HOME/zsh/zcompdump"
 if [[ ! -f "${ZINIT[BIN_DIR]}/zinit.zsh" ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
     command mkdir -p "${ZINIT[HOME_DIR]}" && command chmod g-rwX "${ZINIT[HOME_DIR]}"
-    command git clone https://github.com/zdharma/zinit "${ZINIT[BIN_DIR]}" && \
+    command git clone https://github.com/zdharma-continuum/zinit "${ZINIT[BIN_DIR]}" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -20,16 +20,16 @@ autoload -Uz _zinit
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node
+    zdharma-continuum/z-a-as-monitor \
+    zdharma-continuum/z-a-patch-dl \
+    zdharma-continuum/z-a-bin-gem-node
 
 ### End of Zinit's installer chunk
 
 zinit light-mode for \
     anyakichi/fzf-utils \
     reegnz/jq-zsh-plugin \
-    zdharma/fast-syntax-highlighting \
+    zdharma-continuum/fast-syntax-highlighting \
     zsh-users/zsh-autosuggestions \
     zsh-users/zsh-completions \
     zsh-users/zsh-history-substring-search
