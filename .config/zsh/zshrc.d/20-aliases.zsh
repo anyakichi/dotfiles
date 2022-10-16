d618d56fdb7fb7864c44ce17bf4c6865c633d9fc
INAJIMA Daisuke <inajima@gmail.com>
2022-10-16 14:10:45 +0900

    zsh: Don't use egrep

--- .config/zsh/zshrc.d/20-aliases.zsh
+++ .config/zsh/zshrc.d/20-aliases.zsh
@@ -16,7 +16,7 @@ alias mkdir='nocorrect mkdir'
 alias mv='nocorrect mv'
 alias rm='nocorrect rm'
 
-alias grep='egrep'
+alias grep='grep -E'
 alias vimdiff='vim +next "+execute \"DirDiff\" argv(0) argv(1)"'
 alias ag='ag --pager "less -FRX"'
 alias picocom='picocom -e \\'
