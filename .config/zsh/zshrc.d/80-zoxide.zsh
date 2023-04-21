4f1c7eb33b3d3c573cb5933bd443e30074dcc823
INAJIMA Daisuke <inajima@gmail.com>
2023-04-21 16:57:47 +0900

    zsh: Map C-/ to zoxide

--- .config/zsh/zshrc.d/80-zoxide.zsh
+++ .config/zsh/zshrc.d/80-zoxide.zsh
@@ -1,1 +1,9 @@
-command -v zoxide &>/dev/null && eval "$(zoxide init zsh)"
+if command -v zoxide &>/dev/null; then
+    eval "$(zoxide init zsh)"
+    zoxide-zi-widget() {
+        __zoxide_zi
+        zle reset-prompt
+    }
+    zle -N zoxide-zi-widget
+    bindkey '^_' zoxide-zi-widget
+fi
