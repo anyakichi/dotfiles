316d35a241d7b9184b446e610dc27b48b62e09af
INAJIMA Daisuke <inajima@gmail.com>
2023-04-22 19:09:18 +0900

    zsh: Fix zoxide configuration

--- .config/zsh/zshrc.d/80-zoxide.zsh
+++ .config/zsh/zshrc.d/80-zoxide.zsh
@@ -1,8 +1,12 @@
 if command -v zoxide &>/dev/null; then
     eval "$(zoxide init zsh)"
     zoxide-zi-widget() {
-        __zoxide_zi
-        zle reset-prompt
+        if [[ -n "${BUFFER}" ]]; then
+            fzf-utils::file-widget
+        else
+            __zoxide_zi
+            zle reset-prompt
+        fi
     }
     zle -N zoxide-zi-widget
     bindkey '^_' zoxide-zi-widget
