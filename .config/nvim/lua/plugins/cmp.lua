4b144a3902f8c82fa52f382a97b1d853cd05fe32
INAJIMA Daisuke <inajima@gmail.com>
2024-05-09 12:56:01 +0900

    nvim: Update the luasnip definition

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -14,12 +14,14 @@ return {
       dependencies = {
         {
           "L3MON4D3/LuaSnip",
+          version = "v2.*",
           dependencies = "rafamadriz/friendly-snippets",
           config = function()
             require("luasnip.loaders.from_lua").lazy_load()
             require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_snipmate").lazy_load()
           end,
+          build = "make install_jsregexp",
         },
       },
     },
