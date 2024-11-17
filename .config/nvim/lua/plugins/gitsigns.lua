76e00ed1aff80b507dd7a83402081fd49209a558
INAJIMA Daisuke <inajima@gmail.com>
2024-11-17 17:54:07 +0900

    nvim: Update URL of gitsigns-yadm

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "VeryLazy",
-  dependencies = "seanbreckenridge/gitsigns-yadm.nvim",
+  dependencies = "purarue/gitsigns-yadm.nvim",
   opts = {
     signs = {
       untracked = { text = "" },
