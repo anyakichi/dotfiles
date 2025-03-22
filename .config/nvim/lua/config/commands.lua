46f5e738312e391bb2126111a30ad394479fcdef
INAJIMA Daisuke <inajima@gmail.com>
2025-03-22 13:30:42 +0900

    nvim: Add bang support for ReadCommand

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,11 +1,18 @@
 vim.api.nvim_create_user_command("ReadCommand", function(opts)
   vim.system(opts.fargs, { text = true }, function(job)
     if job.code == 0 then
+      local output = job.stdout:gsub("\n$", "")
+      local lines = vim.split(output, "\n")
       vim.schedule(function()
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(job.stdout, "\n"))
+        if opts.bang then
+          vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+        else
+          local cursor_line = vim.api.nvim_win_get_cursor(0)[1] - 1
+          vim.api.nvim_buf_set_lines(0, cursor_line, cursor_line, false, lines)
+        end
       end)
     else
       print(("code: %d, stderr: %s"):format(job.code, job.stderr))
     end
   end)
-end, { nargs = "*", complete = "file" })
+end, { nargs = "*", bang = true, complete = "file" })
