c283b2069d5a47adee47e95c43d30125f489da95
INAJIMA Daisuke <inajima@gmail.com>
2025-06-04 23:06:45 +0900

    qutebrowser: Improve hook for disabling IME

--- .config/qutebrowser/hook.py
+++ .config/qutebrowser/hook.py
@@ -1,10 +1,24 @@
 import os
 
-from qutebrowser.api import interceptor
+import qutebrowser.app
+from qutebrowser.keyinput import modeman
+from qutebrowser.utils import usertypes
 
-def on_request(info: interceptor.Request):
-    if info.resource_type == interceptor.ResourceType.main_frame:
+
+def on_leave_mode(mode):
+    if mode in (usertypes.KeyMode.command, usertypes.KeyMode.insert):
         os.system("fcitx5-remote -c")
 
 
-interceptor.register(on_request)
+def on_new_window(window):
+    mode_manager = modeman.instance(window.win_id)
+    mode_manager.left.connect(on_leave_mode)
+
+
+class MyApplication(qutebrowser.app.Application):
+    def __init__(self, args):
+        super().__init__(args)
+        self.new_window.connect(on_new_window)
+
+
+qutebrowser.app.Application = MyApplication
