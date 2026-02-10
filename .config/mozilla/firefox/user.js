0f9ccb39e0392e3e4da51cebb0f832d69e8fd951
INAJIMA Daisuke <inajima@soum.co.jp>
2026-02-10 19:52:11 +0900

    tridactyl: Add tridactylrc

--- .config/mozilla/firefox/user.js
+++ .config/mozilla/firefox/user.js
@@ -1,5 +1,5 @@
 user_pref("browser.startup.page", 3);
-user_pref("sidebar.animation.expand-on-hover.delay-duration-ms", "60");
-user_pref("sidebar.animation.expand-on-hover.duration-ms", "200");
+user_pref("sidebar.animation.expand-on-hover.delay-duration-ms", 60);
+user_pref("sidebar.animation.expand-on-hover.duration-ms", 200);
 user_pref("sidebar.verticalTabs", true);
 user_pref("sidebar.visibility", "expand-on-hover");
