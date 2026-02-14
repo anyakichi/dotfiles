697e7bfaa712ccd2ff560e2e4a94af61014d5eda
INAJIMA Daisuke <inajima@gmail.com>
2026-02-14 20:42:30 +0900

    firefox: Add userChrome.css

--- .config/mozilla/firefox/user.js
+++ .config/mozilla/firefox/user.js
@@ -3,3 +3,4 @@ user_pref("sidebar.animation.expand-on-hover.delay-duration-ms", 60);
 user_pref("sidebar.animation.expand-on-hover.duration-ms", 200);
 user_pref("sidebar.verticalTabs", true);
 user_pref("sidebar.visibility", "expand-on-hover");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
