517e7c6c398d7eed4acb58c92b157b086cc19eb7
INAJIMA Daisuke <inajima@gmail.com>
2026-02-15 14:24:52 +0900

    Remove old configs

--- .config/sh/shrc.d/40-ls_colors.sh
+++ .config/sh/shrc.d/40-ls_colors.sh
@@ -3,6 +3,4 @@
 if which vivid >/dev/null 2>&1; then
     LS_COLORS=$(vivid generate "$HOME/.config/vivid/oceanic-next.yml")
     export LS_COLORS
-elif which dircolors >/dev/null 2>&1 && [ -f "${HOME}/.dircolors" ]; then
-    eval "$(dircolors -b "${HOME}/.dircolors")"
 fi
