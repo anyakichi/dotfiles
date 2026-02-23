c8aa2e1157f44bb34dd07d20fcce9fc52cfa6f59
INAJIMA Daisuke <inajima@soum.co.jp>
2026-02-23 20:18:57 +0900

    nvim: Update nvim-surround

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -1,28 +1,28 @@
 return {
   "kylechui/nvim-surround",
   event = "VeryLazy",
-  init = function()
-    vim.keymap.set("n", "S", "<Plug>(nvim-surround-normal)$", { silent = true })
-    vim.keymap.set("i", "<C-s>", function()
-      require("nvim-surround").buffer_setup({ move_cursor = false })
-      local textobj = vim.fn.getcharstr()
-      local surround = vim.fn.getcharstr()
-      vim.cmd("normal sa" .. textobj .. surround)
-      require("nvim-surround").buffer_setup({ move_cursor = "begin" })
-    end)
-  end,
+  keys = {
+    { "S", "<Plug>(nvim-surround-normal)$", mode = "n" },
+    { "S", "<Plug>(nvim-surround-visual-line)", mode = "x" },
+    { "s", "<Plug>(nvim-surround-normal)", mode = "n" },
+    { "s", "<Plug>(nvim-surround-visual)", mode = "x" },
+    { "ss", "<Plug>(nvim-surround-normal-cur)", mode = "n" },
+    {
+      "<C-s>",
+      function()
+        require("nvim-surround").buffer_setup({ move_cursor = false })
+        local textobj = vim.fn.getcharstr()
+        local surround = vim.fn.getcharstr()
+        vim.cmd("normal sa" .. textobj .. surround)
+        require("nvim-surround").buffer_setup({ move_cursor = "begin" })
+      end,
+      mode = "i",
+    },
+  },
   opts = function()
     local config = require("nvim-surround.config")
 
     return {
-      keymaps = {
-        normal = "s",
-        normal_cur = "ss",
-        normal_line = false,
-        normal_cur_line = false,
-        visual = "s",
-        visual_line = "S",
-      },
       surrounds = {
         b = { add = { "(", ")" } },
         ["k"] = {
