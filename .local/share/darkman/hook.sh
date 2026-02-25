df8dafb363e5d63162d3fb1f3ec93378a99a2007
INAJIMA Daisuke <inajima@gmail.com>
2026-02-25 23:14:32 +0900

    darkman: Update hook script

--- .local/share/darkman/hook.sh
+++ .local/share/darkman/hook.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 gsettings set org.gnome.desktop.interface color-scheme "prefer-$1"
+
+qutebrowser ':config-source'
--- .local/share/darkman/qutebrowser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = dark ]; then
-    qutebrowser ':config-source ~/.config/qutebrowser/oceanic-next.py'
-else
-    qutebrowser ':config-source ~/.config/qutebrowser/oceanic-next-light.py'
-fi
