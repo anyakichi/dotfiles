d2b5adfd65927334337af1bc5542da2adfb8c140
INAJIMA Daisuke <inajima@gmail.com>
2026-03-07 19:16:21 +0900

    darkman: Update hook.sh

--- .local/share/darkman/hook.sh
+++ .local/share/darkman/hook.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+qutebrower_source() {
+    socket="$1"
+    if [ -S "$socket" ]; then
+        echo '{"args": [":config-source"], "target_arg": null, "protocol_version": 1}' | socat - UNIX-CONNECT:"$socket"
+    fi
+}
+
 gsettings set org.gnome.desktop.interface color-scheme "prefer-$1"
 
-qutebrowser ':config-source'
+qutebrower_source "$(find "$XDG_RUNTIME_DIR/qutebrowser" -name 'ipc-*' 2>/dev/null | head -1)"
